0d3580a12771197c9b5d20a6c7962114c6da1185
=armada6199 <moh.abdin27@gmail.com>
2023-05-28 10:25:16 +0300

    fixing the routes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
 import { Route, Routes } from 'react-router-dom';
 import About from './pages/About/About';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './pages/Home/Home';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Routes>
-
+      <Route path='/' element={<Home/>}/>
       <Route path='/About' element = {<About />}/>
-      
       </Routes>
-     
+      <Footer/>
     </div>
   );
 }
--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -6,9 +6,6 @@ import Footer from '../../Components/Footer/Footer'
 export default function About() {
   return (
     <div>
-      <Header />
-      
-      <Footer />
     </div>
   )
 }
