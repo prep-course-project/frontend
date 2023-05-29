9d89fb099c01188bf46375c464cff33ad7ebf27d
FarrahYasin <farah.y.yassin@gmail.com>
2023-05-29 07:33:11 +0000

    about us need some css

---
+++ src/pages/About/Partner.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+export default function Partner() {
+  return (
+    <>
+      <div className='about-h2'>
+      <h2> Our Home Partner</h2>
+      </div>
+      {/* <hr class='about-hr1'></hr> */}
+      <div className='about-wrapper'>  
+      
+       <div className='about-partner'>
+         <div className='about-partner-mem'>
+            <img src='https://i.pinimg.com/originals/c2/c3/ff/c2c3ff0e69932bdb50736dad671ae69b.png' alt='company1'/>
+            <h4>First. company</h4>
+            <p>real estate compaany</p>
+         </div>
+         <div className='about-partner-mem'>
+            <img src='https://cdnap.gandalf.tech/agentportal/wp-content/uploads/2021/08/Body-Image-06-Apps-for-agents-1024x640.jpg' alt='company2'/>
+            <h4>second. company</h4>
+            <p>real estate compaany</p>
+         </div>
+
+         <div className='about-partner-mem'>
+            <img src='https://img.freepik.com/premium-vector/elegant-modern-minimalist-real-estate-logo-design-building-construction-logo-real-estate-industry-identity_115973-4367.jpg' alt='company3'/>
+            <h4>third. company</h4>
+            <p>real estate compaany</p>
+         </div>
+
+         <div className='about-partner-mem'>
+            <img src='https://i.pinimg.com/originals/6a/54/de/6a54de8ab9552ccf095e4f3c8e34bf64.jpg' alt='company4'/>
+            <h4>forth. company</h4>
+            <p>real estate compaany</p>
+         </div>
+         </div>
+       </div>
+    </>
+  )
+}
