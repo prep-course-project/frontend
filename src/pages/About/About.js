ce47c87de7dc4908d90906794c626c452503e88d
FarrahYasin <farah.y.yassin@gmail.com>
2023-05-29 19:08:53 +0000

    finished about but need some final css

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,8 +1,5 @@
 import React from 'react'
-import Header from '../../Components/Header/Header'
-import Footer from '../../Components/Footer/Footer'
 import './About.css';
-// import Swipper from './Swipper.';
 import Services from './Services';
 import Info from './Info';
 import Partner from './Partner'
@@ -12,15 +9,12 @@ import Hero from './Hero';
 export default function About() {
   return (
     <>
-       <Header />
        <Hero />
     {/* <Test/> */}
-       {/* <Swipper /> */}
        <Partner />
        <Info />
       <Services/>
       <Member />
-      <Footer />
     </>
   )
 }
