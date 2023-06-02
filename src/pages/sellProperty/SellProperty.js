42386496af2b472edd570ee00d2d912143dd561c
=armada6199 <moh.abdin27@gmail.com>
2023-06-02 14:41:59 +0300

    adding sell property

--- src/pages/sellProperty/SellProperty.js
+++ src/pages/sellProperty/SellProperty.js
@@ -1,31 +1,14 @@
 import React from 'react'
-import SellArea from './SellArea';
-import SellPurpose from './SellPurpose';
-import SellCity from './SellCity'
-import SellPaths from './SellPaths'
-import SellPrice from './SellPrice'
-import SellRooms from './SellRooms'
-import SellType from './SellType'
-import SellHero from './SellHero'
 import SellSection2 from './SellSection2';
-// import TypeSellCard from '../../Components/TypeSellCard/TypeSellCard';
+import SellHero from './SellHero'
 
 export default function SellProperty() {
   
   return (
     <div>
-
       <SellHero/>
       <SellSection2/>
 
-       <SellCity/>
-
-      <SellArea/>
-      <SellPurpose/>
-      <SellPaths/>
-      <SellPrice/>
-      
-      <SellRooms/>
     </div>
   )
 }
