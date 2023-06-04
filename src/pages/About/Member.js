b385d1ef9f6020baed820301aae9d48c41a52356
=armada6199 <moh.abdin27@gmail.com>
2023-06-04 10:43:02 +0300

    adding images

--- src/pages/About/Member.js
+++ src/pages/About/Member.js
@@ -10,7 +10,7 @@ export default function Member() {
       </div>
       <div className='about-team-container'>
          <div className='about-team-box'> 
-         <img src ='https://as2.ftcdn.net/v2/jpg/05/45/89/41/1000_F_545894172_fLINXPGJs19SgFvA3P6vTvXN59iScZJ0.jpg' alt='team image1'/>
+         <img src ='https://media.discordapp.net/attachments/1112031625048490026/1114802327463940186/me.jpg' alt='team image1'/>
           <div className='about-mem-try'></div>
            <h3 >Moh.Abdeen</h3>
            <p>software Engineer</p>
@@ -30,7 +30,7 @@ export default function Member() {
 
            </div>
             <div className='about-team-box'>
-             <img src ='https://www.ultimatebeaver.com/wp-content/uploads/2021/04/photo-gallery-img-02.jpg' alt='team image1'/> 
+             <img src ='https://media.discordapp.net/attachments/1112031625048490026/1114801562234138654/my_img.jpg?width=668&height=671' alt='team image1'/> 
              <div className='about-mem-try'></div>
 
              <h3>Ehab Salhi</h3> 
@@ -52,7 +52,7 @@ export default function Member() {
              </div> 
              
              <div className='about-team-box'>
-             <img src ='https://careercenter.alquds.edu/wp-content/uploads/2020/05/team-1.jpg' alt='team imag1e'/> 
+             <img src ='https://media.discordapp.net/attachments/1111962571994574919/1114803093339643954/image.png' alt='team imag1e'/> 
              <div className='about-mem-try'></div>
 
              <h3>Moh.Jadaan</h3> 
@@ -73,7 +73,7 @@ export default function Member() {
 
              </div> 
              <div className='about-team-box'>
-             <img src ='https://media.istockphoto.com/id/1207856403/photo/portrait-of-happy-african-american-young-businessman-in-formal-suit.jpg?s=612x612&w=0&k=20&c=5KcNoy0GCEXQiNgRYL0FcsWi81-OrXn8D0btadopIEk=' alt='team image1'/> 
+             <img src ='https://media.discordapp.net/attachments/1111962571994574919/1114802680557215817/image.png'/> 
              <div className='about-mem-try'></div>
                <h3>Omar Hamada</h3>
                <p>software Engineer  </p>
@@ -93,7 +93,7 @@ export default function Member() {
 
                 </div> 
                 <div className='about-team-box'> 
-                <img src ='https://st3.depositphotos.com/1662991/33647/i/600/depositphotos_336475472-stock-photo-portrait-happy-good-looking-female.jpg' alt='team image1'/>
+                <img src ='https://media.discordapp.net/attachments/1111962571994574919/1114803687680917524/image.png' alt='team image1'/>
                 <div className='about-mem-try'></div>
 
                  <h3>Farah Yasin</h3>
